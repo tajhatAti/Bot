357a2bdbe95eddad2e49c490a749ee6341266fc4
tajhatAti <n56262ltd@gmail.com>
2026-07-04 09:35:11 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "")
+PORT = int(os.environ.get("PORT", 8080))
